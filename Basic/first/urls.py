7c6e02ee49cffac8049064c17fe8202ac7c05a5c
razzaksr <razzaksr@gmail.com>
2021-12-23 13:53:50 +0530

    login

--- Basic/first/urls.py
+++ Basic/first/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.hai),
     path('info',views.hello),
     path('page',views.greet),
-    path('data',views.send)
+    path('data',views.send),
+    path('log',views.verify)
 ]
--- Basic/first/views.py
+++ Basic/first/views.py
@@ -14,3 +14,14 @@ def greet(request):
 
 def send(request):
     return render(request,'passing.html',{"just":"Hello Zealous!"})
+
+def verify(request):
+    if request.method=="POST":
+        data1=request.POST['user']
+        data2=request.POST['pass']
+        if data1=="aarthi" and data2=="annakili":
+            return render(request,'home.html',{"msg":"Logged successfully"})
+        else:
+            return render(request,'login.html',{"msg":"Login failed"})
+    else:
+        return render(request,'login.html',{"msg":"Login welcomes you"})
